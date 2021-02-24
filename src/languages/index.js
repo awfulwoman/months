a1e1fdc8c2e4e30e6d261ac3a63953831e8b8068
Chazza <github@whalecoiner.com>
2021-02-24 21:14:37 +0100

    Add nl language

--- src/languages/index.js
+++ src/languages/index.js
@@ -1,6 +1,7 @@
 const en = require('./en')
 const de = require('./de')
+const nl = require('./nl')
 
 module.exports = {
-  ...en, ...de
+  ...en, ...de, ...nl
 }
---
+++ src/languages/nl/index.js
@@ -0,0 +1,18 @@
+const monthTable = {
+  nl: [
+    { abbr: 'jan', name: 'januari', number: 1 },
+    { abbr: 'feb', name: 'februari', number: 2 },
+    { abbr: 'maart', name: 'maart', number: 3 },
+    { abbr: 'apr', name: 'april', number: 4 },
+    { abbr: 'mei', name: 'mei', number: 5 },
+    { abbr: 'juni', name: 'juni', number: 6 },
+    { abbr: 'juli', name: 'juli', number: 7 },
+    { abbr: 'aug', name: 'augustus', number: 8 },
+    { abbr: 'sep', name: 'september', number: 9 },
+    { abbr: 'okt', name: 'oktober', number: 10 },
+    { abbr: 'nov', name: 'november', number: 11 },
+    { abbr: 'dec', name: 'december', number: 12 }
+  ]
+}
+
+module.exports = monthTable
