d8fab12582429c389a843b6d7c53380839afc179
Chazza <github@whalecoiner.com>
2021-02-24 18:27:25 +0100

    Fix tests

--- src/__tests__/index.test.js
+++ src/__tests__/index.test.js
@@ -26,30 +26,29 @@ describe('Check parameter values', () => {
   })
 
   test('Pass on strings that are months', () => {
-    expect(months('january')).toEqual({abbr: 'jan', name: 'january', number: 1, index: 0})
-    expect(months('mar')).toEqual({abbr: 'mar', name: 'march', number: 3, index: 2})
+    expect(months('january')).toEqual({ abbr: 'jan', name: 'january', number: 1 })
+    expect(months('mar')).toEqual({ abbr: 'mar', name: 'march', number: 3 })
   })
 
   test('Pass on strings that are months, no matter the case', () => {
-    expect(months('jAnUARy')).toEqual({abbr: 'jan', name: 'january', number: 1, index: 0})
-    expect(months('Mar')).toEqual({abbr: 'mar', name: 'march', number: 3, index: 2})
+    expect(months('jAnUARy')).toEqual({ abbr: 'jan', name: 'january', number: 1 })
+    expect(months('Mar')).toEqual({ abbr: 'mar', name: 'march', number: 3 })
   })
 
-  test('To pass with months that have same name and abbr', () => {
-    expect(months('may')).toEqual({abbr: 'may', name: 'may', number: 5, index: 4})
-    expect(months('May')).toEqual({abbr: 'may', name: 'may', number: 5, index: 4})
+  test('Pass with months that have same name and abbr', () => {
+    expect(months('may')).toEqual({ abbr: 'may', name: 'may', number: 5 })
+    expect(months('May')).toEqual({ abbr: 'may', name: 'may', number: 5 })
   })
 })
 
-
 describe('Check options', () => {
   test('Fail when languages are not strings', () => {
-    expect(months('January', {language: 345})).toBe(false)
-    expect(months('January', {language: {}})).toBe(false)
+    expect(months('January', { language: 345 })).toBe(false)
+    expect(months('January', { language: {} })).toBe(false)
   })
 
   test('Fail on unsupported languages', () => {
-    expect(months('January', {language: 'gr'})).toBe(false)
-    expect(months('January', {language: 'be'})).toBe(false)
+    expect(months('January', { language: 'gr' })).toBe(false)
+    expect(months('January', { language: 'be' })).toBe(false)
   })
 })
