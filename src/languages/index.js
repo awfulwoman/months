c02bc90d0bbb28d590894cd028ad590a2f7b40a5
Chazza <github@whalecoiner.com>
2021-02-25 14:15:53 +0100

    Add italian language

--- src/languages/index.js
+++ src/languages/index.js
@@ -1,7 +1,8 @@
 const en = require('./en')
 const de = require('./de')
+const it = require('./it')
 const nl = require('./nl')
 
 module.exports = {
-  ...en, ...de, ...nl
+  ...en, ...de, ...nl, ...it
 }
---
+++ src/languages/it/index.js
@@ -0,0 +1,18 @@
+const monthtable = {
+  it: [
+    { abbr: 'gen', name: 'gennaio', number: 1 },
+    { abbr: 'feb', name: 'febbraio', number: 2 },
+    { abbr: 'mar', name: 'marzo', number: 3 },
+    { abbr: 'apr', name: 'aprile', number: 4 },
+    { abbr: 'mag', name: 'maggio', number: 5 },
+    { abbr: 'giu', name: 'giugno', number: 6 },
+    { abbr: 'lug', name: 'luglio', number: 7 },
+    { abbr: 'ago', name: 'agosto', number: 8 },
+    { abbr: 'set', name: 'settembre', number: 9 },
+    { abbr: 'ott', name: 'ottobre', number: 10 },
+    { abbr: 'nov', name: 'novembre', number: 11 },
+    { abbr: 'dic', name: 'dicembre', number: 12 }
+  ]
+}
+
+module.exports = monthtable
